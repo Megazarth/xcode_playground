4574870700e8a67ff91321376e5742039ff02495
Yafet Sutanto <ysutanto22@Gmail.com>
2019-05-08 16:00:29 +0700

    iterate array different coding

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,6 @@
 var projects = ["2019 - iOS Apps", "2018 - Mac Apps", "2017 - Web Design"]
 
-for project in projects {
-    print(project)
+for i in 0..<projects.count {
+    print("\(i+1). \(projects[i])")
 }
+
